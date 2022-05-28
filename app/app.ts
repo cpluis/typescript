268c2830dcd82b830791e4349e8dcc08df6fe6dc
Luis Mauricio Carabajal Pires <luis.pires@softdesign.com.br>
2022-05-28 01:20:32 -0300

    começando a estruturar o projeto para typescript

---
+++ app/app.ts
@@ -0,0 +1,8 @@
+import {Negociacao} from './models/negociacao.js';
+
+const negociacao = new Negociacao(new Date(), 100);
+
+console.log(negociacao.volume);
+
+
+https://cursos.alura.com.br/course/typescript-evoluindo-javascript/task/91965
